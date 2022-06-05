f8df2dc0afdbe654794000d5e11cdec5d5c13392
krystic <ice@krystic.com>
2022-06-05 15:59:50 +0800

    update

--- custom_git.sh
+++ custom_git.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+echo "src-link krystic ../../packages" >> feeds.conf.default 
 
-git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git package/luci-app-bandwidthd
-git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
-git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
-git clone https://github.com/1wrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
-git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
-git clone https://github.com/honwen/luci-app-aliddns package/luci-app-aliddns
+#git clone https://github.com/AlexZhuo/luci-app-bandwidthd.git package/luci-app-bandwidthd
+#git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
+#git clone https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
+#git clone https://github.com/1wrt/luci-app-ikoolproxy.git package/luci-app-ikoolproxy
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/honwen/luci-app-aliddns package/luci-app-aliddns
